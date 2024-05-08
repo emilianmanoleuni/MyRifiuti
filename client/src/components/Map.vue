2744e8080e8ba3b77c494eb75ea919f5b96357ce
leorigo2 <leorigo41@gmail.com>
2024-05-08 14:47:30 +0200

    Added Google API

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,16 +1,36 @@
 <template>
     <h1>Map</h1>
+    <div>
+        <div id="Gmap" style="height: 100%;">
+
+        </div>
+    </div>
     <button @click="navigateTo({name: 'homepage'})">HomePage</button>
     <button @click="navigateTo({name: 'map'})">Map</button>
     <br>
 </template>
 
 <script>
+
 export default{
+    data(){
+        return{
+            map: null
+        }
+    },
+    mounted(){
+        this.initMap()
+    },
     methods: {
         navigateTo (route){
             this.$router.push(route)
         },
+        initMap(){
+            this.map = new google.maps.Map(document.getElementById('Gmap'), {
+                center: { lat: 100, lng: 100 },
+                zoom: 8
+            })
+        }
     }
 }
 </script>
