46ed60224fed9050c1992578ab1a560cd692d63b
leorigo2 <112388548+leorigo2@users.noreply.github.com>
2024-05-11 19:40:41 +0000

    Moved marker coordinates to database + added API to GET coordinates

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,20 +1,31 @@
 <template>
-    <h1>Map</h1>
-    <h1 v-if="$store.state.isUserLoggedIn">Sei loggato</h1>
-    <div>
-        <div id="Gmap">
+    <v-app>
+      <v-app-bar app dense>
+        <v-img src="/logo.png" class="logoImage" contain height="50" alt="Logo"></v-img>
+        <v-toolbar-title>MyRifiuti</v-toolbar-title>
+        <v-spacer></v-spacer> <!-- This pushes the menu items to the right -->
+        <v-btn class="topButton" text :to="{ name: 'homepageregistereduser' }">Calendario</v-btn>
+        <v-btn class="topButton" text :to="{ name: '' }">Gruppi</v-btn>
+        <v-btn class="topButton" text :to="{ name: '' }">Mappa</v-btn>
+        <v-btn class="topButton" text :to="{ name: '' }">Segnalazioni</v-btn>
+      </v-app-bar>
 
+      <v-main>
+        <router-view></router-view>
+        <div>
+            <div id="Gmap">
+
+            </div>
         </div>
-    </div>
-    <button @click="navigateTo({name: 'homepage'})">HomePage</button>
-    <button @click="navigateTo({name: 'map'})">Map</button>
+      </v-main>
+    </v-app>
+    
     <br>
 </template>
 
 <script>
 import { Loader } from '@googlemaps/js-api-loader'
-
-import store from '@/store/store';
+import DatabaseService from '../services/DatabaseService'
 
 export default{
     data(){
@@ -36,50 +47,15 @@ export default{
                 east: 11.1938382,
                 west: 11.0214333, 
             },
-            markers: [
-                {
-                    lat: 46.10545314902608, 
-                    lng: 11.101276873613214,
-                    label: "CRM Gardolo"
-                },
-                {
-                    lat: 46.08891705707912, 
-                    lng: 11.135367335099385,
-                    label: "CRM Argentario"
-                },
-                {
-                    lat: 46.063658777361454, 
-                    lng: 11.150853685743687,
-                    label: "CRM Povo"
-                },
-                {
-                    lat: 46.01504102186188, 
-                    lng: 11.12441783438013,
-                    label: "CRM Mattarello"
-                },
-                {
-                    lat: 46.063658777167184, 
-                    lng: 11.109654954791342,
-                    label: "Centro integrato"
-                },
-                {
-                    lat: 46.08723727053129, 
-                    lng: 11.049230151597078,
-                    label: "CRM Sopramonte"
-                }
-            ],
         };
     },
-    created(){
+    mounted(){
         this.loader
         .load()
         .then((google) => {
-            const window = new google.maps.InfoWindow();
-            const map = new google.maps.Map( 
-                document.getElementById("Gmap"),
-                this.mapOptions 
-            );
-            this.markers.forEach((marker) => {
+            DatabaseService.getMarker()
+            .then(markerArray => {
+                markerArray.forEach((marker) => {
                 const mapMarker = new google.maps.Marker({
                                     position: {
                                     lat: marker.lat,
@@ -94,6 +70,15 @@ export default{
                     window.open(mapMarker.getMap(), mapMarker);
                 })
             });
+            })
+            .catch(msg => {
+                console.log(msg);
+            });
+            const window = new google.maps.InfoWindow();
+            const map = new google.maps.Map( 
+                document.getElementById("Gmap"),
+                this.mapOptions 
+            );
             const pathImg = "../../public/";
             const borders = new google.maps.GroundOverlay(pathImg.concat("borders.png"), this.imageBounds);
             if(this.$store.state.isUserLoggedIn){
@@ -101,7 +86,6 @@ export default{
                 highlightedBorders.setMap(map);
             }
             borders.setMap(map);
-            console.log(this.$store.state.user)
         })
         .catch((msg) => console.log(msg) );
     },
@@ -116,8 +100,15 @@ export default{
 <style scoped>
 
 #Gmap {
-    height: 500px;
-    width: 1000px;
+    height: 850px;
+    width: 2000px;
+}
+
+.logoImage{
+        margin-right: 20px;
+    }
+.topButton{
+    margin-right: 10px;
 }
 
 </style>
--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -2,8 +2,22 @@ import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import colors from 'vuetify/util/colors'
 
 export default createVuetify({
     components,
     directives,
+    theme: {
+        themes: {
+          light: {
+            dark: false,
+            colors: {
+              background: colors.green.lighten5, //#E8F5E9
+              basic: colors.green.lighten3, 
+              buttons: colors.green.darken1, 
+              buttonsLight: colors.green.lighten3
+            }
+          },
+        },
+      },
 })
