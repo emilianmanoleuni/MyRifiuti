a4ce6707f10bfd5a49f42ad47d99aa815a115440
leorigo2 <112388548+leorigo2@users.noreply.github.com>
2024-06-09 12:28:44 +0000

    Code fix

--- client/src/components/Groups.vue
+++ client/src/components/Groups.vue
@@ -83,28 +83,6 @@
           <v-card-title class="text-h6 soonOrLoginContent">Please Login first:</v-card-title>
           <v-btn variant="elevated" color="buttons" :to="{ name: 'login' }">Login</v-btn>
       </v-card>
-  </v-container>
-    <v-container class="mainBody">
-        <v-row>
-            <v-col cols="2">
-                <v-card align="center">
-                    <v-card-title class="text-h5">Gruppi</v-card-title>
-                </v-card>
-            </v-col>
-            <v-col></v-col>
-            <v-col cols="4"></v-col>
-        </v-row>
-        <v-row cols="12">
-            <v-col cols="12">
-                <v-card v-if="isUserLoggedIn" class="soonOrLoginBox" align="center">
-                    <v-card-title class="text-h6 soonOrLoginContent">Soon</v-card-title>
-                </v-card>
-                <v-card v-if="!isUserLoggedIn" class="soonOrLoginBox" align="center">
-                    <v-card-title class="text-h6 soonOrLoginContent">Please Login first:</v-card-title>
-                    <v-btn variant="elevated" color="buttons" :to="{ name: 'login' }">Login</v-btn>
-                </v-card>
-            </v-col>
-        </v-row>
     </v-container>
 </template>
 
