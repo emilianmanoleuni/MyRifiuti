c4bfe677b4ad5e296583106d6edb6c898d074c4b
emilianmanoleuni <121548465+emilianmanoleuni@users.noreply.github.com>
2024-06-11 15:11:51 +0200

    Update Api.js

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/apiv2`,
-    //baseURL: `https://myrifiuti.onrender.com/api`,
+    //baseURL: `http://localhost:8081/apiv2`,
+    baseURL: `https://myrifiuti.onrender.com/apiv2`,
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}` //Token on LocalStorage
     }
