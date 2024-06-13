535613daa668557a2e2a86a09ee5c99635e4f0d0
emilianmanoleuni <121548465+emilianmanoleuni@users.noreply.github.com>
2024-06-13 10:21:38 +0200

    Changed Backend Host

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    //baseURL: `http://localhost:8081/apiv2`,
-    baseURL: `https://myrifiuti.onrender.com/apiv2`,
+    //baseURL: `http://localhost:8081/apiv2`, FOR LOCAL
+    //baseURL: `https://myrifiuti.onrender.com/apiv2`, OLD No more pipeline avaible
+    baseURL: `https://myrifiuti-t8yo.onrender.com/apiv2`
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}` //Token on LocalStorage
     }
